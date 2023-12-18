7e601a53ee124a5a14aa0834043bf6ef192f538e
MarcosRosales24 <marcos@sicar.mx>
2023-12-18 11:44:13 -0600

    commit vergas

--- test-library/build.gradle.kts
+++ test-library/build.gradle.kts
@@ -1,47 +1,13 @@
 plugins {
     kotlin("jvm") version "1.9.21"
-    `maven-publish`
-    signing
 }
 
 group = "com.test"
-version = "1.0.0-SNAPSHOT"
-
-
-
+version = "1.0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
-
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["kotlin"])
-            groupId = "com.test"
-            artifactId = "test-library"
-            version = "1.0.0"
-        }
-    }
-
-
-    repositories {
-        maven {
-            // Aquí debes colocar la URL del repositorio al que estás subiendo tu paquete
-            // Por ejemplo, para subir a Maven Central, necesitarías configurar esto con la URL de Sonatype
-            // Además, necesitas configurar las credenciales para autenticarte en el repositorio remoto
-            url = uri("https://github.com/MarshallSICAR/test-library.git")
-            credentials {
-                username = "Marcos"
-                password = System.getenv("MAVEN_SECRET")
-            }
-        }
-    }
-}
-
-signing {
-    sign(publishing.publications["mavenJava"])
+    mavenLocal()
 }
 
 
